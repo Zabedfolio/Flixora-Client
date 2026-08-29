dd21f47b0c07e82b9eda9527950c27d5d9d68494
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-08-29 19:50:03 +0600

    fix(auth): add google user avatar domain to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'image.tmdb.org',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 };
